e3c97e2e97be7d76f6ecb12399500572cca50a74
sonygoud21 <sadhana152001@gmail.com>
2023-08-30 18:12:18 +0530

    getelementbyclass

--- example/DOM.js
+++ example/DOM.js
@@ -1,32 +1,41 @@
-var header =document.querySelector('#main-header');
-header.style.borderBottom='solid 5px #ccc';
+//var header =document.querySelector('#main-header');
+//header.style.borderBottom='solid 5px #ccc';
 
-var input=document.querySelector('input');
+//var input=document.querySelector('input');
 
-input.vaue='Hello World';
-var submit=document.querySelector('input[type="submit"]');
-submit.value="send";
-var secondItem=document.querySelector('li:nth-child(odd)');
-secondItem.style.color="green";
+//input.vaue='Hello World';
+//var submit=document.querySelector('input[type="submit"]');
+//submit.value="send";
+//var secondItem=document.querySelector('li:nth-child(odd)');
+//secondItem.style.color="green";
 
-var third=document.getElementsByClassName("list-group-item");
-third[2].style.backgroundColor='green';
-var al=document.getElementById("items");
+//var third=document.getElementsByClassName("list-group-item");
+//third[2].style.backgroundColor='green';
+//var al=document.getElementById("items");
 
-al.style.fontWeight='bold';
+//al.style.fontWeight='bold';
 
-var id=document.getElementById('items');
-id.createElement('li');
+//var id=document.getElementById('items');
+//id.createElement('li');
 
 
-var item=document.querySelector('.list-group-item:nth-child(2)');
-item.style.backgroundcolor='green';
-var item=document.querySelector('.list-group-item:nth-child(3)');
+//var item=document.querySelector('.list-group-item:nth-child(2)');
+//item.style.backgroundcolor='green';
+//var item=document.querySelector('.list-group-item:nth-child(3)');
 
-item.style.visibility='hidden';
+//item.style.visibility='hidden';
 
-var it=document.querySelectorAll('.list-group-item:nth-child(2)');
-it.style.backgroundcolor='green';
+//var it=document.querySelectorAll('.list-group-item:nth-child(2)');
+//it.style.backgroundcolor='green';
 
-var irt=document.querySelectorAll('.list-group-item:nth-child(odd)');
-irt.style.backgroundcolor='green';
+//var irt=document.querySelectorAll('.list-group-item:nth-child(odd)');
+//irt.style.backgroundcolor='green';
+
+var items =document.getElementsByClassName('list-group-item');
+ console.log(items);
+console.log(items[1]);
+
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+
+items[1].style.backgroundColor='yellow';
